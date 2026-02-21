3632c53f58b481f8d46951a72e501b7c95f9a62d
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-02-21 21:12:11 +0000

    Stuff and things

---
+++ api/fafauth-ap-south-1/handler.py
@@ -0,0 +1,12 @@
+import json
+
+
+def custom_authorizer(event, context):
+    body = {
+        "message": "Go Serverless v4.0! Your function executed successfully!",
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
