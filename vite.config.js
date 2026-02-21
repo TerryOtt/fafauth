0874e83eafdeb375b24c3d7737d5eb398ae43974
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-02-21 17:09:32 +0000

    Now launching HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [basicSsl()],
   server: {
     port: 3000,
+    https: true
   },
 });
+
