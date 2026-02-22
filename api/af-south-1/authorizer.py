8c4df6c55cd0b322b615f2bb1e28512458ea219e
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-02-22 19:33:37 +0000

    Reading JWKS out of Param Store worked first shot

--- api/af-south-1/authorizer.py
+++ api/af-south-1/authorizer.py
@@ -1,8 +1,13 @@
+import boto3
 import json
 import jwt
 import logging
 import time
 
+
+aws_region: str = "af-south-1"
+
+ssm_client = boto3.client('ssm')
 
 logger = logging.getLogger()
 logger.setLevel('DEBUG')
@@ -47,11 +52,37 @@ def custom_authorizer(event, context):
     logger.info(f"Got bearer token \"{bearer_token}\"")
 
     try:
+        # Read JWKS from Kinde with public key of public/private pair used to sign all bearer tokens
+        parameter_store_response = ssm_client.get_parameter( Name=client_token_signing_jwks_param_store_path )
+    except:
+        logger.error("Exception thrown in Param Store Read")
+        return {
+            'isAuthorized': is_authorized,
+            'context': {
+                'auth_time_ms': int((time.perf_counter() - start_time) * ms_in_s)
+            },
+        }
+
+    if not 'Parameter' in parameter_store_response and not 'Value' in parameter_store_response['Parameter']:
+        logger.error("Parameter store response did not have a value")
+        
+        return {
+            'isAuthorized': is_authorized,
+            'context': {
+                'auth_time_ms': int((time.perf_counter() - start_time) * ms_in_s)
+            },
+        }
+
+    jwks_value: str = parameter_store_response['Parameter']['Value']
+    logger.info("Got JWKS string")
+    logger.info( json.dumps(json.loads(jwks_value), indent=4, sort_keys=True) )
+
+    
+    try:
         payload: dict[str, typing.Any] = jwt.decode(bearer_token, options={'verify_signature': False})
         is_authorized = True
     except:
-        logger.warn("Exception thrown in JWT decode")
-        pass
+        logger.error("Exception thrown in JWT decode")
 
     return {
         'isAuthorized': is_authorized,
